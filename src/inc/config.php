fdcfe3d857b211d85ec45bd4f8c9d30ec1b6850d
Andrew <andre.petrovto@mail.ru>
2024-11-23 23:17:56 +0300

    docs: update readme

---
+++ src/inc/config.php
@@ -0,0 +1,7 @@
+<?php
+
+define('CLIENT_URL', '<YOUR_ACC_URL>/api/v4');
+define('SECRET_TOKEN', '<SECRET_TOKEN>');
+define('CLIENT_ID', '<INTEGRATIN_ID');
+define('LONG_TERM_TOKEN', '<LONG_TERM_TOKEN>');
+define('ACCESS_TOKEN', '<ACCESS_TOKEN');
